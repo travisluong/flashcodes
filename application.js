8e2b3f7314eac69289dfa9c66a1e73e0581143db
Travis Luong <luongtravis@gmail.com>
2015-06-02 17:26:51 -0700

    clean code, separate actions from helpers, bigger report card

--- application.js
+++ application.js
@@ -14,6 +14,9 @@ var fc = {
   userOptionsDiv: $('#user-options'),
   shuffleButton: $('#shuffle'),
   backFirstButton: $('#back-first'),
+  rightColDiv: $('#right-col'),
+  gameAreaDiv: $('#game-area'),
+  reportCardDiv: $('#report-card'),
 
   // constants
   LEFT_ARROW_KEY_CODE: 37,
@@ -47,7 +50,7 @@ var fc = {
     this.url = args.url;
   },
 
-  // events: user action --> triggers event --> calls controller action
+  // events: user action --> triggers event --> calls action
   // --------------------------------------------------------------------------------
   bindCards: function() {
     fc.cardsDiv.on('click', '.card', function() {
@@ -55,16 +58,15 @@ var fc = {
     });
   },
 
-  bindDeck: function() {
-    fc.decksDiv.on('click', 'button', function(e) {
-      var name = $(this).data('name');
-      fc.loadDeck(name);
-    });    
+  bindCardsControls: function() {
+    fc.cardsControlsDiv.on('click', '#wrong', fc.incrementWrong);
+    fc.cardsControlsDiv.on('click', '#correct', fc.incrementCorrect);
   },
 
-  bindCardsControls: function() {
-    fc.cardsControlsDiv.on('click', '#wrong', fc.wrongCallback);
-    fc.cardsControlsDiv.on('click', '#correct', fc.correctCallback);
+  bindDeck: function() {
+    fc.decksDiv.on('click', 'button', function(e) {
+      fc.loadDeck(e);
+    });    
   },
 
   bindKeyPress: function() {
@@ -86,7 +88,151 @@ var fc = {
     fc.bindUserOptions();
   },
 
-  // views: input data --> render function --> output html
+  // actions. stuff that happens in response to events.
+  // --------------------------------------------------------------------------------
+
+  handleKeyPress: function(e) {
+    if (!fc.gameStarted) {
+      return;
+    }
+    if (e.keyCode === fc.DOWN_ARROW_KEY_CODE) {
+      e.preventDefault();
+      fc.toggleCard();
+    } else if (e.keyCode === fc.RIGHT_ARROW_KEY_CODE ) {
+      fc.incrementCorrect();
+    } else if (e.keyCode === fc.LEFT_ARROW_KEY_CODE) {
+      fc.incrementWrong();
+    }      
+  },
+
+  incrementCorrect: function() {
+    fc.currentCardIndex += 1;
+    fc.currentCard.isCorrect = true;
+    fc.correctCount += 1;
+    fc.updateGameInfo();
+    fc.nextCardLogic();
+  },
+
+  incrementWrong: function() {
+    fc.currentCardIndex += 1;
+    fc.currentCard.isCorrect = false;
+    fc.wrongCount += 1;
+    fc.updateGameInfo(fc.current);
+    fc.nextCardLogic();  
+  },
+
+  loadDeck: function(e) {
+    var name = $(e.target).data('name');
+    fc.loadDeckByName(name);
+  },
+
+  toggleShuffle: function() {
+    fc.userOptions.shuffle = !fc.userOptions.shuffle;
+    fc.shuffleButton.toggleClass('btnOn', fc.userOptions.shuffle);
+  },
+
+  toggleBackFirst: function() {
+    fc.userOptions.backFirst = !fc.userOptions.backFirst;
+    fc.backFirstButton.toggleClass('btnOn', fc.userOptions.backFirst);
+  },
+
+  toggleCard: function() {
+    fc.cardsDiv.find('.front').slideToggle();
+    fc.cardsDiv.find('.back').slideToggle();
+  },  
+
+  // helpers. helps keep code DRY. 
+  // encapsulate code that is called in multiple actions.
+  // --------------------------------------------------------------------------------
+
+  fetchDecks: function(fn) {
+    $.getJSON("decks.json", function(data) {
+      var decksArray = data.map(function(obj) {
+        return new fc.Deck(obj);
+      });
+      fn(decksArray);
+    });
+  },
+
+  fetchCards: function(deck, fn) {
+    $.getJSON(deck.url, function(data) {
+      var cards = data.map(function(obj) {
+        return new fc.Card(obj);
+      });
+      fn(cards);
+    });
+  },
+
+  loadDeckByName: function(name) {
+    var deck = fc.decks.find(function(element, index, array) {
+      if (element.name === name) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    fc.cardsDiv.empty();
+    fc.currentDeck = deck;
+    fc.fetchCards(deck, function(cards) {
+      if (fc.userOptions.shuffle) {
+        cards = fc.shuffle(cards);
+      }
+      fc.currentCards = cards;
+      fc.loadCard(fc.currentCards[0]);
+      fc.currentCardIndex = 0;
+      fc.correctCount = 0;
+      fc.wrongCount = 0;
+      fc.updateGameInfo();
+      fc.reportCardDiv.empty();
+      fc.gameAreaDiv.show();
+      fc.gameStarted = true;
+    });
+  },
+
+  updateGameInfo: function() {
+    fc.gameInfoDiv.empty();
+    var gameInfoFragment = fc.renderGameInfo(fc.currentCardIndex, 
+        fc.currentCards.length, 
+        fc.correctCount, 
+        fc.wrongCount);
+    fc.gameInfoDiv.append(gameInfoFragment);
+  },
+
+  loadCard: function(card) {
+    fc.currentCard = card;
+    var cardFragment = fc.renderCard(card, fc.userOptions.backFirst);
+    fc.cardsDiv.append(cardFragment);
+  },
+
+  loadDecks: function(deckArray) {
+    fc.decks = deckArray;
+    var decksFragment = fc.renderDecks(deckArray);
+    fc.decksDiv.append(decksFragment);
+  },
+
+  nextCardLogic: function() {
+    if (fc.currentCardIndex < fc.currentCards.length) {
+      fc.showNextCard();
+    } else {
+      fc.gameStarted = false;
+      var reportCard = fc.renderReportCard(fc.currentCards, fc.correctCount);
+      fc.cardsDiv.empty();
+      fc.gameAreaDiv.hide();
+      fc.reportCardDiv.append(reportCard);
+    }
+  },
+
+  showNextCard: function() {
+    fc.cardsDiv.find('.card').remove();
+    fc.loadCard(fc.currentCards[fc.currentCardIndex]);
+  },
+
+  shuffle: function(o){
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+  },
+
+  // views. input data --> output html
   // --------------------------------------------------------------------------------
   renderCard: function(card, backFirst) {
     if (!backFirst) {
@@ -120,14 +266,6 @@ var fc = {
     return html;
   },
 
-  renderCardsControls: function() {
-    var html = '<div>' + 
-      '<button id="wrong">wrong</button>' +
-      '<button id="correct">correct</button>' +
-      '</div>';
-    return html;
-  },
-
   renderGameInfo: function(cardIndex, cardsLength, correctCount, wrongCount) {
     var html = '<span class="orange">Progress: ' + cardIndex + '/' + cardsLength + '</span> ' +
       '<span class="wrong">Wrong: ' + wrongCount + '</span> ' +
@@ -155,155 +293,8 @@ var fc = {
       html += fc.renderCardRow(card);
     });
     html += '</tbody></table>' +
-      "<p class='percentage'>Percentage:" + percentage + " </p>";
+      "<p class='percentage'>Percentage: " + percentage + " </p>";
     return html;
-  },
-
-  // controllers: controller actions --> update models, views, and app state
-  // --------------------------------------------------------------------------------
-  handleKeyPress: function(e) {
-    if (!fc.gameStarted) {
-      return;
-    }
-    if (e.keyCode === fc.DOWN_ARROW_KEY_CODE) {
-      e.preventDefault();
-      fc.toggleCard();
-    } else if (e.keyCode === fc.RIGHT_ARROW_KEY_CODE ) {
-      fc.correctCallback();
-    } else if (e.keyCode === fc.LEFT_ARROW_KEY_CODE) {
-      fc.wrongCallback();
-    }      
-  },
-
-  toggleShuffle: function() {
-    fc.userOptions.shuffle = !fc.userOptions.shuffle;
-    fc.shuffleButton.toggleClass('btnOn', fc.userOptions.shuffle);
-  },
-
-  toggleBackFirst: function() {
-    fc.userOptions.backFirst = !fc.userOptions.backFirst;
-    fc.backFirstButton.toggleClass('btnOn', fc.userOptions.backFirst);
-  },
-
-  toggleCard: function() {
-    fc.cardsDiv.find('.front').slideToggle();
-    fc.cardsDiv.find('.back').slideToggle();
-  },
-
-  fetchDecks: function(fn) {
-    $.getJSON("decks.json", function(data) {
-      var decksArray = data.map(function(obj) {
-        return new fc.Deck(obj);
-      });
-      fn(decksArray);
-    });
-  },
-
-  fetchCards: function(deck, fn) {
-    $.getJSON(deck.url, function(data) {
-      var cards = data.map(function(obj) {
-        return new fc.Card(obj);
-      });
-      fn(cards);
-    });
-  },
-
-  loadDeckCallback: function() {
-    fc.loadCard(fc.currentCards[0]);
-    fc.updateCardsControls();
-    fc.resetGameInfo();
-    fc.updateGameInfo();
-    fc.gameStarted = true;
-  },
-
-  loadDeck: function(name) {
-    var deck = fc.decks.find(function(element, index, array) {
-      if (element.name === name) {
-        return true;
-      } else {
-        return false;
-      }
-    });
-    fc.cardsDiv.empty();
-    fc.currentDeck = deck;
-    fc.fetchCards(deck, function(cards) {
-      if (fc.userOptions.shuffle) {
-        cards = fc.shuffle(cards);
-      }
-      fc.currentCards = cards;
-      fc.loadDeckCallback();
-    });
-  },
-
-  updateCardsControls: function() {
-    fc.cardsControlsDiv.empty();
-    var cardsControlsFragment = fc.renderCardsControls();
-    fc.cardsControlsDiv.append(cardsControlsFragment);
-  },
-
-  updateGameInfo: function() {
-    fc.gameInfoDiv.empty();
-    var gameInfoFragment = fc.renderGameInfo(fc.currentCardIndex, 
-        fc.currentCards.length, 
-        fc.correctCount, 
-        fc.wrongCount);
-    fc.gameInfoDiv.append(gameInfoFragment);
-  },
-
-  loadCard: function(card) {
-    fc.currentCard = card;
-    var cardFragment = fc.renderCard(card, fc.userOptions.backFirst);
-    fc.cardsDiv.append(cardFragment);
-  },
-
-  loadDecks: function(deckArray) {
-    fc.decks = deckArray;
-    var decksFragment = fc.renderDecks(deckArray);
-    fc.decksDiv.append(decksFragment);
-  },
-
-  wrongCallback: function() {
-    fc.currentCardIndex += 1;
-    fc.currentCard.isCorrect = false;
-    fc.wrongCount += 1;
-    fc.updateGameInfo(fc.current);
-    fc.nextCardLogic();  
-  },
-
-  correctCallback: function() {
-    fc.currentCardIndex += 1;
-    fc.currentCard.isCorrect = true;
-    fc.correctCount += 1;
-    fc.updateGameInfo();
-    fc.nextCardLogic();
-  },
-
-  nextCardLogic: function() {
-    if (fc.currentCardIndex < fc.currentCards.length) {
-      fc.showNextCard();
-    } else {
-      fc.gameStarted = false;
-      var reportCard = fc.renderReportCard(fc.currentCards, fc.correctCount);
-      fc.cardsDiv.empty();
-      fc.cardsControlsDiv.empty();
-      fc.cardsDiv.append(reportCard);
-    }
-  },
-
-  showNextCard: function() {
-    fc.cardsDiv.find('.card').remove();
-    fc.loadCard(fc.currentCards[fc.currentCardIndex]);
-  },
-
-  resetGameInfo: function() {
-    fc.currentCardIndex = 0;
-    fc.correctCount = 0;
-    fc.wrongCount = 0;
-  },
-
-  shuffle: function(o){
-    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-    return o;
   },
 
   // init: starting point of app
@@ -311,7 +302,7 @@ var fc = {
   init: function() {
     fc.fetchDecks(function(deckArray) {
       fc.loadDecks(deckArray);
-      fc.loadDeck(fc.decks[0].name);
+      fc.loadDeckByName(fc.decks[0].name);
       fc.bindInit();
       fc.gameStarted = true;
     });
